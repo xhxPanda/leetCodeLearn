0e6c8eff84f35da054ca4a0a95941fe3a2419d88
XieHongXin <171510228@qq.com>
2020-12-13 18:55:52 +0800

    (leetcode整理)：整理11-20题

--- src/onetohundred/elevenToTwenty/DivideTwoInteger.java
+++ src/onetohundred/elevenToTwenty/DivideTwoInteger.java
@@ -7,24 +7,24 @@ public class DivideTwoInteger {
         }
         boolean isNeg = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0);
 
-        if(dividend == Integer.MIN_VALUE && divisor == -1) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return ~dividend;
         }
 
         long dividend1 = Math.abs((long) dividend);
-        long divisor1 = Math.abs((long)divisor);
+        long divisor1 = Math.abs((long) divisor);
 
-        int result= 0;
-        while(dividend >= divisor1) {
+        int result = 0;
+        while (dividend >= divisor1) {
             int shiftNum = 0;
-            while(dividend1 >= divisor1 << shiftNum) {
+            while (dividend1 >= divisor1 << shiftNum) {
                 shiftNum++;
             }
-            result += 1 << (shiftNum-1);
-            dividend1 -= divisor1<<(shiftNum-1);
+            result += 1 << (shiftNum - 1);
+            dividend1 -= divisor1 << (shiftNum - 1);
         }
 
-        if(isNeg) {
+        if (isNeg) {
             result = -result;
         }
         return result;
