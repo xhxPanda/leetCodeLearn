01b01e1edd88c7d19af0b355bbf7da02f9b4b4e0
xhxpanda <171510228@qq.com>
2021-01-28 16:39:33 +0800

    整理代码

---
+++ src/janchallange/SmallestStringGivenNumericValue.java
@@ -0,0 +1,34 @@
+package janchallange;
+
+import java.util.Arrays;
+
+public class SmallestStringGivenNumericValue {
+    public String getSmallestString(int n, int k) {
+        char[] chs = new char[n];
+        Arrays.fill(chs, 'a');
+
+        k -= n;
+
+        for (int i = chs.length - 1; i >=0;i--) {
+            if(k <=0) {
+                break;
+            }
+
+            if (k >= 'z' - chs[i]) {
+                k -= 'z' - chs[i];
+                chs[i] = 'z';
+            } else {
+                chs[i] += k;
+                k = 0;
+            }
+        }
+
+        return new String(chs);
+    }
+
+    public static void main(String[] args) {
+        SmallestStringGivenNumericValue a = new SmallestStringGivenNumericValue();
+        a.getSmallestString(5, 73);
+
+    }
+}
