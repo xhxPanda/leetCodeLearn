413455799dc33c6b387cff7dad6e683ac5984611
xhxpanda <171510228@qq.com>
2021-02-04 21:30:41 +0800

    （1月leetCode挑战）：

---
+++ src/janchallange/LongestHarmoniousSubsequence.java
@@ -0,0 +1,23 @@
+package janchallange;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class LongestHarmoniousSubsequence {
+    public int findLHS(int[] nums) {
+       HashMap<Integer, Integer> numCount = new HashMap<Integer, Integer>();
+         for (int num : nums) {
+             numCount.put(num, numCount.getOrDefault(num, 0) + 1);
+         }
+         
+         int maxLen = 0;
+         Set<Integer> keys = numCount.keySet();
+         for (int key : keys) {
+             if (numCount.containsKey(key + 1)) {
+                 maxLen = Math.max(maxLen, numCount.get(key + 1) + numCount.get(key));
+             }
+         }
+         
+         return maxLen;
+    }
+}
