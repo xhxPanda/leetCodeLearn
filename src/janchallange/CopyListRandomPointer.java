09d0de782a74d43c5588e32356da6169405e29d4
XieHongXin <171510228@qq.com>
2021-02-11 14:37:46 +0800

    (leetcode整理)：鼠年最后一波提交2月挑战整理代码，老实说，这三题我都看不懂

---
+++ src/janchallange/CopyListRandomPointer.java
@@ -0,0 +1,46 @@
+package janchallange;
+
+public class CopyListRandomPointer {
+
+    private Node copyRandomList(Node head) {
+        if (head == null) return null;
+        Node head2, p2, p = head;
+        while (p != null) {
+            Node n = new Node(p.val);
+            n.next = p.next;
+            p.next = n;
+            p = n.next;
+        }
+        p = head;
+        while (p != null) {
+            if (p.random != null) {
+               p.next.random = p.random.next;
+            }
+            p = p.next.next;
+        }
+
+        head2 = head.next;
+        p = head;
+        while (p != null) {
+            p2 = p.next;
+            p.next = p2.next;
+            if (p2.next != null) {
+                p2.next = p2.next.next;
+            }
+            p = p.next;
+        }
+        return head2;
+    }
+
+   private class Node {
+        int val;
+        Node next;
+        Node random;
+
+    public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
