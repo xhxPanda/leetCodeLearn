bbb7f928c23b177d9a4a612605f6cd515c6ea786
xhxpanda <171510228@qq.com>
2021-02-23 11:55:07 +0800

    （2月leetCode挑战）：

--- src/febchallange/BrokenCalculator.java
+++ src/febchallange/BrokenCalculator.java
@@ -4,14 +4,16 @@ public class BrokenCalculator {
     public int brokenCalc(int X, int Y) {
         int count = 0;
         while (Y != X) {
-            if (X >Y) return X - Y + count;
-            
-            if (Y % 2 != 0) Y += 1;
-            else Y /= 2;
-            
+            if (X > Y) return X - Y + count;
+
+            if (Y % 2 != 0) {
+                Y += 1;
+            } else {
+                Y /= 2;
+            }
             count++;
         }
-        
+
         return count;
     }
 }
