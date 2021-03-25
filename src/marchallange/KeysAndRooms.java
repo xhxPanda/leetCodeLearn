0a3db2c9fe30068f35f8c3918dcfde9d0ca64347
xhxpanda <171510228@qq.com>
2021-03-25 20:28:30 +0800

    （3月leetCode挑战）：整理

---
+++ src/marchallange/KeysAndRooms.java
@@ -0,0 +1,26 @@
+package marchallange;
+
+import java.util.List;
+
+public class KeysAndRooms {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(rooms, 0, visited);
+
+        for (boolean b : visited) {
+            if (b == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void dfs(List<List<Integer>> rooms, int i, boolean[] visited) {
+        visited[i] = true;
+        for (Integer key : rooms.get(i)) {
+            if (!visited[key]) {
+                dfs(rooms, key, visited);
+            }
+        }
+    }
+}
