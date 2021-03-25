cd019b3554de7e0fe8cf48aedf9758d7eaf66914
xhxpanda <171510228@qq.com>
2021-03-25 10:27:58 +0800

    （3月leetCode挑战）：整理

---
+++ src/marchallange/EncodeDecodeTinyURLSolution.java
@@ -0,0 +1,28 @@
+package marchallange;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EncodeDecodeTinyURLSolution {
+    Map<String, String> map = new HashMap<>();
+
+    public String encode(String longUrl) {
+        char[] arr = new char[2];
+        arr[0] = longUrl.charAt(0);
+        arr[1] = longUrl.charAt(longUrl.length() - 1);
+        String d = new String(arr);
+        map.put(longUrl, d);
+        return d;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        String s="";
+        for(Map.Entry<String , String> entry : map.entrySet())
+        {
+            if(entry.getValue().equals(shortUrl))
+                s=  s+entry.getKey();
+        }
+        return s;
+    }
+}
